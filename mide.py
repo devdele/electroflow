daed90a4349d3f37bb261470b647b93ea5e727dc
devdele <111873932+devdele@users.noreply.github.com>
2024-06-08 12:19:35 +0100

    Add files via upload

---
+++ mide.py
@@ -0,0 +1,17 @@
+# spam = ['apples', 'bananas', 'tofus', 'cats']
+
+# def num(names):       
+#        count = ", ".join(names[:-1])
+#        print("{count}, and  {names[-1]}")
+
+
+# nam = ["are", " tolu" , "Emma"]
+# num(nam)
+
+
+def num(names):
+    count = ", ".join(names[:-1])
+    print(f"{count}, and {names[-1]}")
+
+nam = ["are", "tolu", "Emma"]
+num(nam)
